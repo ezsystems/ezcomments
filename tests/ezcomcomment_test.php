826291bb064f5c97571a2f20167cce3a3b967f62
Łukasz Serwatka <ls@ez.no>
2009-12-08 13:57:23 +0000

    - Updated unit tests
    git-svn-id: http://svn.projects.ez.no/ezcomments/trunk@7 20a62680-2ad1-4a8a-825d-218b1b6ec9b5

--- tests/ezcomcomment_test.php
+++ tests/ezcomcomment_test.php
@@ -16,7 +16,7 @@ class ezcomCommentTest extends ezpDatabaseTestCase
     public function __construct()
     {
         parent::__construct();
-        $this->setName( "ezcommComment object test " );
+        $this->setName( "ezcommComment object test" );
     }
 
     public function setUp()
@@ -27,58 +27,57 @@ class ezcomCommentTest extends ezpDatabaseTestCase
     }
 
     /**
-     * 1. insert 1 row
-     * 2. fetch one row , vertify the result
+     * 1. Create a new comment object with dummy data
+     * 2. Check if data are stored properly
+     * 
      */
-    public function testFetchObject()
+    public function testCreateObject()
     {
-        $db = eZDB::instance();
-        //1. insert 1 row
-        $query = "INSERT INTO ezcomment( contentobject_id,
-                                        language_id,
-                                        created,
-                                        modified,
-                                        user_id,
-                                        session_key,
-                                        ip,
-                                        name,
-                                        email,
-                                        url,
-                                        text,
-                                        notification
-                                        )
-                                VALUES( 12,
-                                        2,
-                                        21213423,
-                                        21321231,
-                                        01,
-                                        'dfsfsfafdaf',
-                                        '10.0.2.122',
-                                        'xc',
-                                        'xc@ez.no',
-                                        'http://ez.no',
-                                        'hello, this is a test!',
-                                        1
-                                        )";
-        $db->query($query);
-        $db->commit();
-        //2. fetch the list
-        $result = $db->arrayQuery( "SELECT id FROM ezcomment ORDER BY id DESC LIMIT 0,1" );
-        $id = $result[0]['id'];
-        $comment = ezcomComment::fetch( $id );
+        // Create a new comment
+        $comment = ezcomComment::create();
+        $comment->setAttribute( 'contentobject_id', 12 );
+        $comment->setAttribute( 'language_id', 2 );
+        $comment->setAttribute( 'created', 21213423 );
+        $comment->setAttribute( 'modified', 21321231 );
+        $comment->setAttribute( 'user_id', 14 );
+        $comment->setAttribute( 'session_key', 'a2e4822a98337283e39f7b60acf85ec9' );
+        $comment->setAttribute( 'ip', '10.0.2.122' );
+        $comment->setAttribute( 'name', 'xc' );
+        $comment->setAttribute( 'email', 'xc@ez.no' );
+        $comment->setAttribute( 'url', 'http://ez.no' );
+        $comment->setAttribute( 'text', 'ezcomComment object test comment.' );
+        $comment->setAttribute( 'notification', 1 );
+        $comment->store();
+
+        $this->assertType( 'ezcomComment', $comment );
         $this->assertEquals( 12, $comment->attribute( 'contentobject_id' ) );
         $this->assertEquals( 2, $comment->attribute( 'language_id' ) );
         $this->assertEquals( 21213423, $comment->attribute( 'created' ) );
         $this->assertEquals( 21321231, $comment->attribute( 'modified' ) );
-        $this->assertEquals( 01, $comment->attribute( 'user_id' ) );
-        $this->assertEquals( 'dfsfsfafdaf', $comment->attribute( 'session_key' ) );
+        $this->assertEquals( 14, $comment->attribute( 'user_id' ) );
+        $this->assertEquals( 'a2e4822a98337283e39f7b60acf85ec9', $comment->attribute( 'session_key' ) );
         $this->assertEquals( '10.0.2.122', $comment->attribute( 'ip' ) );
         $this->assertEquals( 'xc', $comment->attribute( 'name' ) );
         $this->assertEquals( 'xc@ez.no', $comment->attribute( 'email' ) );
         $this->assertEquals( 'http://ez.no', $comment->attribute( 'url' ) );
-        $this->assertEquals( 'hello, this is a test!', $comment->attribute( 'text' ) );
+        $this->assertEquals( 'ezcomComment object test comment.', $comment->attribute( 'text' ) );
         $this->assertEquals( 1, $comment->attribute( 'notification' ) );
-        $db->query( 'DELETE FROM ezcomment WHERE id='. $id );
+    }
+
+    /**
+     * 1. Fetch ezcomComment object
+     * 2. Check is object is instance of ezcomComment class
+     * 3. Fetch ezcomComment object which does not exist
+     * 4. Check if result equals to null
+     *
+     */
+    public function testFetchObject()
+    {
+        $comment = ezcomComment::fetch( 1 );
+        $this->assertType( 'ezcomComment', $comment );
+        
+        $comment = ezcomComment::fetch( 2 );
+        $this->assertEquals( null, $comment );
     }
 }
 ?>
