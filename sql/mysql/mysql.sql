653749ba594a83f9177bc29412d831126bd63a8f
Xiongjie Chen <xc@ez.no>
2009-12-17 17:35:26 +0000

    - Added missed 'title' field in ezcomment table
    git-svn-id: http://svn.projects.ez.no/ezcomments/trunk@37 20a62680-2ad1-4a8a-825d-218b1b6ec9b5

--- sql/mysql/mysql.sql
+++ sql/mysql/mysql.sql
@@ -15,6 +15,7 @@ CREATE TABLE IF NOT EXISTS `ezcomment` (
   `text` text NOT NULL,
   `status` int(11) NOT NULL,
   `notification` smallint(6) NOT NULL DEFAULT '0',
+  `title` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id_session_key_ip` (`user_id`,`session_key`,`ip`),
   KEY `contentobject_id_contentobject_attribute_id_lang_id_parentcom_id` (`contentobject_id`,`contentobject_attribute_id`,`language_id`,`parent_comment_id`)
