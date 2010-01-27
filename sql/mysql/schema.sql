c575c49b01769f3305ee3d45f86d29179eb1ab78
Ole Marius Smestad <oms@ez.no>
2010-01-27 09:06:02 +0000

    - Updated: "Flattened " schema, and generic and postgresql.
    git-svn-id: http://svn.projects.ez.no/ezcomments/trunk@103 20a62680-2ad1-4a8a-825d-218b1b6ec9b5

--- sql/mysql/schema.sql
+++ sql/mysql/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `ezcomment` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `language_id` int(11) NOT NULL,
   `created` int(11) NOT NULL,
   `modified` int(11) NOT NULL,
@@ -8,13 +8,13 @@ CREATE TABLE IF NOT EXISTS `ezcomment` (
   `ip` varchar(100) NOT NULL,
   `contentobject_id` int(11) NOT NULL,
   `contentobject_attribute_id` int(11) NOT NULL,
-  `parent_comment_id` bigint(20) NOT NULL DEFAULT '0',
+  `parent_comment_id` int(11) NOT NULL DEFAULT '0',
   `name` varchar(255) NOT NULL,
   `email` varchar(75) NOT NULL,
   `url` varchar(255) NOT NULL,
   `text` text NOT NULL,
   `status` int(11) NOT NULL,
-  `notification` smallint(6) NOT NULL DEFAULT '0',
+  `notification` int(11) NOT NULL DEFAULT '0',
   `title` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id_session_key_ip` (`user_id`,`session_key`,`ip`),
@@ -22,32 +22,32 @@ CREATE TABLE IF NOT EXISTS `ezcomment` (
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `ezcomment_notification` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `contentobject_id` int(11) NOT NULL,
   `language_id` int(11) NOT NULL,
   `send_time` int(11) NOT NULL DEFAULT 0,
-  `status` smallint(6) NOT NULL DEFAULT '1',
+  `status` int(11) NOT NULL DEFAULT '1',
   `comment_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `ezcomment_subscriber` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `enabled` smallint(6) NOT NULL DEFAULT '1',
+  `enabled` int(11) NOT NULL DEFAULT '1',
   `hash_string` varchar(50),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `ezcomment_subscription` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(20) NOT NULL,
-  `subscriber_id` bigint(20) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `subscriber_id` int(11) NOT NULL,
   `subscription_type` varchar(30) NOT NULL,
   `content_id` varchar(100) NOT NULL,
   `subscription_time` int(11) NOT NULL,
-  `enabled` smallint(6) NOT NULL DEFAULT 1,
+  `enabled` int(11) NOT NULL DEFAULT 1,
   `hash_string` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
---
+++ sql/postgresql/schema.sql
@@ -0,0 +1,189 @@
+
+
+
+
+
+
+
+
+
+
+
+CREATE SEQUENCE ezcomment_s
+    START 1
+    INCREMENT 1
+    MAXVALUE 9223372036854775807
+    MINVALUE 1
+    CACHE 1;
+
+
+
+
+
+
+
+CREATE SEQUENCE ezcomment_notification_s
+    START 1
+    INCREMENT 1
+    MAXVALUE 9223372036854775807
+    MINVALUE 1
+    CACHE 1;
+
+
+
+
+
+
+
+CREATE SEQUENCE ezcomment_subscriber_s
+    START 1
+    INCREMENT 1
+    MAXVALUE 9223372036854775807
+    MINVALUE 1
+    CACHE 1;
+
+
+
+
+
+
+
+CREATE SEQUENCE ezcomment_subscription_s
+    START 1
+    INCREMENT 1
+    MAXVALUE 9223372036854775807
+    MINVALUE 1
+    CACHE 1;
+
+
+
+
+
+
+
+CREATE TABLE ezcomment (
+    contentobject_attribute_id integer DEFAULT 0 NOT NULL,
+    contentobject_id integer DEFAULT 0 NOT NULL,
+    created integer DEFAULT 0 NOT NULL,
+    email character varying(75) DEFAULT ''::character varying NOT NULL,
+    id integer DEFAULT nextval('ezcomment_s'::text) NOT NULL,
+    ip character varying(100) DEFAULT ''::character varying NOT NULL,
+    language_id integer DEFAULT 0 NOT NULL,
+    modified integer DEFAULT 0 NOT NULL,
+    name character varying(255) DEFAULT ''::character varying NOT NULL,
+    notification integer DEFAULT 0 NOT NULL,
+    parent_comment_id integer DEFAULT 0 NOT NULL,
+    session_key character varying(32) DEFAULT ''::character varying NOT NULL,
+    status integer DEFAULT 0 NOT NULL,
+    text text NOT NULL,
+    title character varying(255),
+    url character varying(255) DEFAULT ''::character varying NOT NULL,
+    user_id integer DEFAULT 0 NOT NULL
+);
+
+
+
+
+
+
+
+CREATE TABLE ezcomment_notification (
+    comment_id integer DEFAULT 0 NOT NULL,
+    contentobject_id integer DEFAULT 0 NOT NULL,
+    id integer DEFAULT nextval('ezcomment_notification_s'::text) NOT NULL,
+    language_id integer DEFAULT 0 NOT NULL,
+    send_time integer DEFAULT 0 NOT NULL,
+    status integer DEFAULT 1 NOT NULL
+);
+
+
+
+
+
+
+
+CREATE TABLE ezcomment_subscriber (
+    email character varying(50) DEFAULT ''::character varying NOT NULL,
+    enabled integer DEFAULT 1 NOT NULL,
+    hash_string character varying(50),
+    id integer DEFAULT nextval('ezcomment_subscriber_s'::text) NOT NULL,
+    user_id integer DEFAULT 0 NOT NULL
+);
+
+
+
+
+
+
+
+CREATE TABLE ezcomment_subscription (
+    content_id character varying(100) DEFAULT ''::character varying NOT NULL,
+    enabled integer DEFAULT 1 NOT NULL,
+    hash_string character varying(50),
+    id integer DEFAULT nextval('ezcomment_subscription_s'::text) NOT NULL,
+    subscriber_id integer DEFAULT 0 NOT NULL,
+    subscription_time integer DEFAULT 0 NOT NULL,
+    subscription_type character varying(30) DEFAULT ''::character varying NOT NULL,
+    user_id integer DEFAULT 0 NOT NULL
+);
+
+
+
+
+
+
+
+CREATE INDEX contentobject_id_contentobject_attribute_id_lang_id_parentcom_id ON ezcomment USING btree (contentobject_id, contentobject_attribute_id, language_id, parent_comment_id);
+
+
+
+
+
+
+
+CREATE INDEX user_id_session_key_ip ON ezcomment USING btree (user_id, session_key, ip);
+
+
+
+
+
+
+
+
+ALTER TABLE ONLY ezcomment
+    ADD CONSTRAINT ezcomment_pkey PRIMARY KEY (id);
+
+
+
+
+
+
+
+ALTER TABLE ONLY ezcomment_notification
+    ADD CONSTRAINT ezcomment_notification_pkey PRIMARY KEY (id);
+
+
+
+
+
+
+
+ALTER TABLE ONLY ezcomment_subscriber
+    ADD CONSTRAINT ezcomment_subscriber_pkey PRIMARY KEY (id);
+
+
+
+
+
+
+
+ALTER TABLE ONLY ezcomment_subscription
+    ADD CONSTRAINT ezcomment_subscription_pkey PRIMARY KEY (id);
+
+
+
+
+
+
+
+
