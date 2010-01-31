a0937db127ae8c1da6ad4605a6c6fb701eb7143e
Ole Marius Smestad <oms@ez.no>
2010-01-31 01:30:28 +0000

    - CS: whitespace
    git-svn-id: http://svn.projects.ez.no/ezcomments/trunk@144 20a62680-2ad1-4a8a-825d-218b1b6ec9b5

--- classes/ezcomfunctioncollection.php
+++ classes/ezcomfunctioncollection.php
@@ -32,18 +32,18 @@
 class ezcomFunctionCollection
 {
 
- static function fetchCommentList( $contentObjectID, $languageID, $sortField, $sortOrder, $length )
- {
-    $sort = array( $sortField=>$sortOrder );
-    $result = ezcomComment::fetchByContentObjectID( $contentObjectID, $languageID, $sort, 0, $length );
-    return array( 'result' => $result );
- }
+    public static function fetchCommentList( $contentObjectID, $languageID, $sortField, $sortOrder, $length )
+    {
+        $sort = array( $sortField=>$sortOrder );
+        $result = ezcomComment::fetchByContentObjectID( $contentObjectID, $languageID, $sort, 0, $length );
+        return array( 'result' => $result );
+    }
 
-static function fetchCommentCount( $contentObjectID, $languageID )
- {
-    $result = ezcomComment::countByContent( $contentObjectID, $languageID );
-    return array( 'result' => $result );
- }
+    public static function fetchCommentCount( $contentObjectID, $languageID )
+    {
+        $result = ezcomComment::countByContent( $contentObjectID, $languageID );
+        return array( 'result' => $result );
+    }
 }
 
 ?>
