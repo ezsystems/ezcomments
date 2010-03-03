12b958fb3161c6ae2c42acb994efb25f4870efe5
Xiongjie Chen <xc@ez.no>
2010-03-03 08:56:36 +0000

    - Added German translation (made by Georg Franz)
    git-svn-id: http://svn.projects.ez.no/ezcomments/trunk@235 20a62680-2ad1-4a8a-825d-218b1b6ec9b5

--- translations/ger-DE/translation.ts
+++ translations/ger-DE/translation.ts
@@ -1,316 +1,312 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="de_DE" sourcelanguage="en">
 <context>
     <name>ezcomments/comment/action</name>
     <message>
         <source>Back</source>
-        <translation type="unfinished"></translation>
+        <translation>Zurück</translation>
     </message>
     <message>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Löschen</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Abbrechen</translation>
     </message>
     <message>
         <source>Update comment</source>
-        <translation type="unfinished"></translation>
+        <translation>Kommentar aktualisieren</translation>
     </message>
 </context>
 <context>
     <name>ezcomments/comment/activate</name>
     <message>
         <source>The is no subscription with the hash string!</source>
-        <translation type="unfinished"></translation>
+        <translation>Kein Abonnement mit diesem Code gefunden!</translation>
     </message>
     <message>
         <source>The subscriber is disabled!</source>
-        <translation type="unfinished"></translation>
+        <translation>Der Abonnent ist deaktiviert!</translation>
     </message>
     <message>
         <source>The subscription is activated!</source>
-        <translation type="unfinished"></translation>
+        <translation>Das Abonnement ist aktiviert!</translation>
     </message>
     <message>
         <source>Go to settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu den Einstellungen</translation>
     </message>
     <message>
         <source>Activate subscription</source>
-        <translation type="unfinished"></translation>
+        <translation>Abonnement aktivieren</translation>
     </message>
 </context>
 <context>
     <name>ezcomments/comment/activationnotification</name>
     <message>
         <source>Dear %email,</source>
-        <translation type="unfinished"></translation>
+        <translation>Hallo %email,</translation>
     </message>
     <message>
         <source>You have subscribed the comment update of content. Before you get notification on the content, you need to confirm the subscription.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sie wollen alle Antworten auf den ausgewählten Content per E-Mail erhalten. Bevor Sie E-Mails zugeschickt bekommen, müssen Sie dies bestätigen.</translation>
     </message>
     <message>
         <source>Please click the link to activate.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bitte klicken Sie auf den Link, um das Abo zu aktivieren</translation>
     </message>
     <message>
         <source>The activation will expire after %expiry_days days.</source>
-        <translation type="unfinished"></translation>
+        <translation>Die Aktivierung wird nach %expiry_days auslaufen.</translation>
     </message>
     <message>
         <source>The activation will expire after %expiry_hours hours.</source>
-        <translation type="unfinished"></translation>
+        <translation>Die Aktivierung wird nach %expiry_hours Stunden auslaufen.</translation>
     </message>
     <message>
         <source>If you don&apos;t want to receive the subscription email, igore this email.</source>
-        <translation type="unfinished"></translation>
+        <translation>Wenn Sie keine Abo E-Mail erhalten wollen, dann brauchen Sie nur diese E-Mail zu ignorieren.</translation>
     </message>
     <message>
         <source>You can go to your setting page to manage your subscription.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sie können auf Ihre Einstellungs-Seite gehen, um das Abonnement zu verwalten.</translation>
     </message>
     <message>
         <source>Subscribe comment</source>
-        <translation type="unfinished"></translation>
+        <translation>Kommentare abonnieren</translation>
     </message>
 </context>
 <context>
     <name>ezcomments/comment/add</name>
     <message>
         <source>Posting succeeds!</source>
-        <translation type="unfinished"></translation>
+        <translation>Kommentar abgeschickt!</translation>
     </message>
     <message>
         <source>Add comment</source>
-        <translation type="unfinished"></translation>
+        <translation>Kommentar hinzufügen</translation>
     </message>
     <message>
         <source>Commenting has been turned off for this content.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>You should not access this view directly.</source>
-        <translation type="unfinished"></translation>
+        <translation>Die Kommentar Funktion wurde für diesen Inhalt deaktiviert.</translation>
     </message>
 </context>
 <context>
     <name>ezcomments/comment/add/form</name>
     <message>
         <source>Post comment</source>
-        <translation type="unfinished"></translation>
+        <translation>Kommentar schreiben</translation>
     </message>
     <message>
         <source>Title:</source>
-        <translation type="unfinished"></translation>
+        <translation>Überschrift:</translation>
     </message>
     <message>
         <source>Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Name</translation>
     </message>
     <message>
         <source>Website:</source>
-        <translation type="unfinished"></translation>
+        <translation>Webseite:</translation>
     </message>
     <message>
         <source>Email:</source>
-        <translation type="unfinished"></translation>
+        <translation>E-Mail:</translation>
     </message>
     <message>
         <source>(The email address will not be shown)</source>
-        <translation type="unfinished"></translation>
+        <translation>(Die E-Mail Adresse wird nicht angezeigt)</translation>
     </message>
     <message>
         <source>Content:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Add comment</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Reset form</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>There is a problem with your comment form </source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Remember me</source>
-        <translation type="unfinished"></translation>
+        <translation>Inhalt:</translation>
     </message>
     <message>
         <source>Notify me of new comments</source>
-        <translation type="unfinished"></translation>
+        <translation>Mich per E-Mail benachrichtigen, wenn neue Kommentare geschrieben werden</translation>
+    </message>
+    <message>
+        <source>Remember me</source>
+        <translation>Einstellungen im Cookie speichern</translation>
+    </message>
+    <message>
+        <source>Add comment</source>
+        <translation>Kommentar schreiben</translation>
+    </message>
+    <message>
+        <source>Reset form</source>
+        <translation>Formularinhalt löschen</translation>
     </message>
     <message>
         <source>Notified of new comments</source>
-        <translation type="unfinished"></translation>
+        <translation>Benachrichtigt bei neuen Kommentaren</translation>
+    </message>
+    <message>
+        <source>There is a problem with your comment form </source>
+        <translation>Es gibt ein Problem beim Kommentar Formular</translation>
     </message>
 </context>
 <context>
     <name>ezcomments/comment/delete</name>
     <message>
-        <source>You don&apos;t have access to delete comment or the comment is hidden!</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Delete comment?</source>
-        <translation type="unfinished"></translation>
+        <translation>Kommentar löschen?</translation>
     </message>
     <message>
         <source>Deleting failed!</source>
-        <translation type="unfinished"></translation>
+        <translation>Löschen fehlgeschlagen!</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to delete comment or the comment is hidden!</source>
+        <translation>Sie haben keine Berechtigung, Kommentare zu löschen oder die Kommentare wurden versteckt!</translation>
     </message>
     <message>
         <source>Delete comment</source>
-        <translation type="unfinished"></translation>
+        <translation>Kommentar löschen</translation>
     </message>
 </context>
 <context>
     <name>ezcomments/comment/edit</name>
     <message>
-        <source>Edit comment</source>
-        <translation type="unfinished"></translation>
+        <source>You don&apos;t have access to edit.</source>
+        <translation>Sie haben keine Berechtigung, den Kommentar zu bearbeiten.</translation>
     </message>
     <message>
-        <source>You don&apos;t have access to edit.</source>
-        <translation type="unfinished"></translation>
+        <source>Edit comment</source>
+        <translation>Kommentar bearbeiten</translation>
     </message>
     <message>
         <source>Updating failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktualisierung fehltgeschlagen.</translation>
     </message>
 </context>
 <context>
     <name>ezcomments/comment/notification</name>
     <message>
         <source>New comment</source>
-        <translation type="unfinished"></translation>
+        <translation>Neuer Kommentar</translation>
     </message>
 </context>
 <context>
     <name>ezcomments/comment/setting</name>
     <message>
-        <source>OK</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Select</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Content</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Subscription time</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Subscribed</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Save</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Reset</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Comment settings</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>There is no subscription.</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine Abonnements vorhanden.</translation>
     </message>
     <message>
         <source>Update succeeds!</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktualisierung erfolgreich!</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Auswählen</translation>
+    </message>
+    <message>
+        <source>Content</source>
+        <translation>Inhalt</translation>
+    </message>
+    <message>
+        <source>Subscription time</source>
+        <translation>Abonnement Dauer</translation>
+    </message>
+    <message>
+        <source>Subscribed</source>
+        <translation>Abonniert</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Speichern</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Zurücksetzen</translation>
+    </message>
+    <message>
+        <source>Comment settings</source>
+        <translation>Kommentar Einstellungen</translation>
     </message>
 </context>
 <context>
     <name>ezcomments/comment/view</name>
     <message>
         <source>wrote:</source>
-        <translation type="unfinished"></translation>
+        <translation>schrieb:</translation>
     </message>
     <message>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Bearbeiten</translation>
     </message>
     <message>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Löschen</translation>
     </message>
     <message>
         <source>There is no comment.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kein Kommentar vorhanden.</translation>
     </message>
     <message>
         <source>You don&apos;t have access to view comment.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sie haben keinen Zugriff zu den Kommentaren.</translation>
     </message>
     <message>
         <source>You don&apos;t have access to post comment.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sie dürfen keine Kommentare veröffentlichen.</translation>
     </message>
     <message>
         <source>View all %total_count comments</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle %total_count Kommentare anzeigen</translation>
     </message>
     <message>
         <source>Total %total_count comments</source>
-        <translation type="unfinished"></translation>
+        <translation>%total_count Kommentare</translation>
     </message>
     <message>
         <source>The page parameter is not a number.</source>
-        <translation type="unfinished"></translation>
+        <translation>Der Seitenparameter ist keine Zahl.</translation>
     </message>
     <message>
         <source>The page parameter can&apos;t be minus.</source>
-        <translation type="unfinished"></translation>
+        <translation>Der Seitenparameter kann nicht negativ sein.</translation>
     </message>
     <message>
         <source>Comment/View</source>
-        <translation type="unfinished"></translation>
+        <translation>Kommentar/Anzeigen</translation>
     </message>
 </context>
 <context>
     <name>ezcomments/comment/view/action</name>
     <message>
         <source>Back</source>
-        <translation type="unfinished"></translation>
+        <translation>Zurück</translation>
     </message>
 </context>
 <context>
     <name>ezcomments/comment/view/page</name>
     <message>
         <source>%current_page/%total_page</source>
-        <translation type="unfinished"></translation>
+        <translation>%current_page/%total_page</translation>
     </message>
     <message>
         <source>Previous page</source>
-        <translation type="unfinished"></translation>
+        <translation>Vorherige Seite</translation>
     </message>
     <message>
         <source>Next page</source>
-        <translation type="unfinished"></translation>
+        <translation>Nächste Seite</translation>
     </message>
     <message>
         <source>Total comments:</source>
-        <translation type="unfinished"></translation>
+        <translation>Anzahl der Kommentare:</translation>
     </message>
     <message>
         <source>Pages:</source>
-        <translation type="unfinished"></translation>
+        <translation>Seiten:</translation>
     </message>
 </context>
 <context>
@@ -318,25 +314,25 @@
     <message>
         <source>Comments</source>
         <comment>Datatype name</comment>
-        <translation type="unfinished"></translation>
+        <translation>Kommentare</translation>
     </message>
 </context>
 <context>
     <name>ezcomments/datatype/edit</name>
     <message>
         <source>Show comments</source>
-        <translation type="unfinished"></translation>
+        <translation>Kommentare anzeigen</translation>
     </message>
     <message>
         <source>Enable commenting</source>
-        <translation type="unfinished"></translation>
+        <translation>Kommentieren einschalten</translation>
     </message>
 </context>
 <context>
     <name>ezcomments/ezwebin/user/edit</name>
     <message>
         <source>My comment settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Meine Kommentar Einstellungen</translation>
     </message>
 </context>
 </TS>
