559a0149755262ebd89d2083542fb8544b541c6d
Nicolas Pastorino <nfrp@ez.no>
2010-03-10 11:27:14 +0000

    --Brazilian translation, credits go to Renan Leme
    git-svn-id: http://svn.projects.ez.no/ezcomments/trunk@269 20a62680-2ad1-4a8a-825d-218b1b6ec9b5

---
+++ translations/por-BR/translation.ts
@@ -0,0 +1,344 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name></name>
+    <message>
+        <source>Comment settings</source>
+        <translation>Configurações de comentários</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezwebin/user/edit</name>
+    <message>
+        <source>My comment settings</source>
+        <translation>Minhas configurações de comentários</translation>
+    </message>
+</context>
+<context>
+    <name>extension/comment/admin</name>
+    <message>
+        <source>Show comments</source>
+        <translation>Exibir comentários</translation>
+    </message>
+    <message>
+        <source>Enable commenting</source>
+        <translation>Habilitar comentários</translation>
+    </message>
+</context>
+<context>
+    <name>extension/comment/delete</name>
+    <message>
+        <source>You don&apos;t have  the permission to delete comment  or the showing comment function is disabled!</source>
+        <translation>Você não tem permissão para excluir comentários ou a função de exbibir comentários está desabilitada!</translation>
+    </message>
+</context>
+<context>
+    <name>extension/comment/setting</name>
+    <message>
+        <source>Comment settings</source>
+        <translation>Configurações de comentários</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Selecionar</translation>
+    </message>
+    <message>
+        <source>Content</source>
+        <translation>Conteúdo</translation>
+    </message>
+    <message>
+        <source>Subscription time</source>
+        <translation>Tempo de inscrição</translation>
+    </message>
+    <message>
+        <source>Subscribed</source>
+        <translation>Inscrito</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Salvar</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Reiniciar</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezcomments/action</name>
+    <message>
+        <source>Back</source>
+        <translation>Voltar</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Apagar</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <source>Update comment</source>
+        <translation>Atualizar comentário</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezcomments/activate</name>
+    <message>
+        <source>Activate subscription</source>
+        <translation>Ativar inscrição</translation>
+    </message>
+    <message>
+        <source>The subscription was activated!</source>
+        <translation>A inscrição foi ativada!</translation>
+    </message>
+    <message>
+        <source>Go to settings</source>
+        <translation>Ir para as configurações</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezcomments/activate/email</name>
+    <message>
+        <source>Dear %email,</source>
+        <translation>Caro(a) %email,</translation>
+    </message>
+    <message>
+        <source>You have subscribed the comment update of content. Before you get notification on the content, you need to confirm the subscription.</source>
+        <translation>Você se inscreveu receber atualizações dos comentário de conteúdo. Antes receber as notificações sobre o conteúdo, você precisa confirmar sua inscrição.</translation>
+    </message>
+    <message>
+        <source>Please click the link to activate.</source>
+        <translation>Por favor clique no link para ativar.</translation>
+    </message>
+    <message>
+        <source>The activation will expire after %expiry_days days.</source>
+        <translation>Esta ativação irá expirar após %expiry_days dias.</translation>
+    </message>
+    <message>
+        <source>The activation will expire after %expiry_hours hours.</source>
+        <translation>Esta ativação irá expirar após  %expiry_hours horas.</translation>
+    </message>
+    <message>
+        <source>If you don&apos;t want to receive the subscription email, igore this email.</source>
+        <translation>Se você não deseja receber os emails com as atualizações, igore este email.</translation>
+    </message>
+    <message>
+        <source>You can go to your setting page to manage your subscription.</source>
+        <translation>Você pode ir para sua página de configuração para configurar este servico.</translation>
+    </message>
+    <message>
+        <source>Subscribe comment</source>
+        <translation>Assinar comentários</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezcomments/add</name>
+    <message>
+        <source>Posting succeeds!</source>
+        <translation>Enviado com sucesso!</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezcomments/commentform</name>
+    <message>
+        <source>Title:</source>
+        <translation>Título:</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>Seu nome:</translation>
+    </message>
+    <message>
+        <source>Website:</source>
+        <translation>Seu site:</translation>
+    </message>
+    <message>
+        <source>Email:</source>
+        <translation>Seu email:</translation>
+    </message>
+    <message>
+        <source>Content:</source>
+        <translation>Comentário:</translation>
+    </message>
+    <message>
+        <source>Notified</source>
+        <translation>Receber notificações de comentários</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezcomments/delete</name>
+    <message>
+        <source>Deleting failed!</source>
+        <translation>A remoção falhou!</translation>
+    </message>
+    <message>
+        <source>Delete comment</source>
+        <translation>Apagar comentário</translation>
+    </message>
+    <message>
+        <source>Delete comment?</source>
+        <translation>Apagar comentário?</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezcomments/edit</name>
+    <message>
+        <source>Edit comment</source>
+        <translation>Editar comentário</translation>
+    </message>
+    <message>
+        <source>Updating failed!</source>
+        <translation>A atualização falhou!</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to edit here!</source>
+        <translation>Você não tem permissão para editar aqui!</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezcomments/notification/email</name>
+    <message>
+        <source>New comment</source>
+        <translation>Novo comentário</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezcomments/setting</name>
+    <message>
+        <source>There is no subscription.</source>
+        <translation>Não há inscrição.</translation>
+    </message>
+    <message>
+        <source>Update succeeds!</source>
+        <translation>Atualizado com sucesso!</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezcomments/view</name>
+    <message>
+        <source>View comment</source>
+        <translation>Ver comentário</translation>
+    </message>
+    <message>
+        <source>wrote:</source>
+        <translation>escreveu</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Editar</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Apagar</translation>
+    </message>
+    <message>
+        <source>There is no comment!</source>
+        <translation>Não há comentários!</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to view comment here!</source>
+        <translation>Você não tem acesso para ver comentários aqui!</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to post comment here!</source>
+        <translation>Você não tem acesso para comentar aqui!</translation>
+    </message>
+    <message>
+        <source>View all %total_count comments</source>
+        <translation>Ver todos os %total_count comentários</translation>
+    </message>
+    <message>
+        <source>Total %total_count comments</source>
+        <translation>Total de %total_count comentários</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezcomments/view/page</name>
+    <message>
+        <source>%current_page/%total_page</source>
+        <translation>%current_page/%total_page</translation>
+    </message>
+    <message>
+        <source>Previous page</source>
+        <translation>Página anterior</translation>
+    </message>
+    <message>
+        <source>Next page</source>
+        <translation>Próxima página</translation>
+    </message>
+    <message>
+        <source>Total comments:</source>
+        <translation>Total de comentários:</translation>
+    </message>
+    <message>
+        <source>Pages:</source>
+        <translation>Páginas:</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/add</name>
+    <message>
+        <source>Add comment</source>
+        <translation>Comentar</translation>
+    </message>
+    <message>
+        <source>Commenting has been turned off for this content.</source>
+        <translation>Os comentários foram desabilitados para este conteúdo.</translation>
+    </message>
+    <message>
+        <source>There is a problem with your comment form </source>
+        <translation>Há um problema com o seu formulário de comentário </translation>
+    </message>
+    <message>
+        <source>Post comment</source>
+        <translation>Deixe seu comentário</translation>
+    </message>
+    <message>
+        <source>Title:</source>
+        <translation>Título:</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>Seu nome:</translation>
+    </message>
+    <message>
+        <source>Website:</source>
+        <translation>Seu site:</translation>
+    </message>
+    <message>
+        <source>Email:</source>
+        <translation>Seu email:</translation>
+    </message>
+    <message>
+        <source>(the email address will not be shown)</source>
+        <translation>(não será divulgado)</translation>
+    </message>
+    <message>
+        <source>Content:</source>
+        <translation>Comentário:</translation>
+    </message>
+    <message>
+        <source>Notified:</source>
+        <translation>Receber notificações de comentários</translation>
+    </message>
+    <message>
+        <source>Remember me:</source>
+        <translation>Lembre-me:</translation>
+    </message>
+    <message>
+        <source>Reset form</source>
+        <translation>Apagar formulário</translation>
+    </message>
+</context>
+<context>
+    <name>view/action</name>
+    <message>
+        <source>Back</source>
+        <translation>Voltar</translation>
+    </message>
+</context>
+</TS>
