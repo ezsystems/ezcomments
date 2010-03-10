f5ec90d67f756e66f89cb89bed8b99c290f1a44a
Xiongjie Chen <xc@ez.no>
2010-03-10 22:27:26 +0000

    - Added eZ Comments website link to ezinfo
    git-svn-id: http://svn.projects.ez.no/ezcomments/trunk@272 20a62680-2ad1-4a8a-825d-218b1b6ec9b5

--- ezinfo.php
+++ ezinfo.php
@@ -12,7 +12,7 @@ class eZCommentsInfo
     static function info()
     {
         return array(
-            'Name' => "eZ Comments",
+            'Name' => "<a href='http://projects.ez.no/ezcomments'>eZ Comments</a>",
             'Version' => "1.0.0RC",
             'Copyright' => 'Copyright (C) 1999-' . date('Y') . ' eZ Systems AS',
             'License' => 'GNU General Public License v2.0',
