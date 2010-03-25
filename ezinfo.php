797a3f2f4375a29639080fbd1aa93c1a858cd4f7
Xiongjie Chen <xc@ez.no>
2010-03-25 13:17:16 +0000

    - Removed RC in ezinfo.php
    git-svn-id: http://svn.projects.ez.no/ezcomments/trunk@295 20a62680-2ad1-4a8a-825d-218b1b6ec9b5

--- ezinfo.php
+++ ezinfo.php
@@ -13,7 +13,7 @@ class eZCommentsInfo
     {
         return array(
             'Name' => "<a href='http://projects.ez.no/ezcomments'>eZ Comments</a>",
-            'Version' => "1.0.0RC",
+            'Version' => "1.0.0",
             'Copyright' => 'Copyright (C) 1999-2010 eZ Systems AS',
             'License' => 'GNU General Public License v2.0',
         );
