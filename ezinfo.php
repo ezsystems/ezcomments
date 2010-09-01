60b953f08ab36604398b2e5717eef1e70c5826f1
Xiongjie Chen <xc@ez.no>
2010-09-01 13:10:10 +0000

    Updated: version number in ezinfo
    git-svn-id: http://svn.projects.ez.no/ezcomments/stable/1.0@333 20a62680-2ad1-4a8a-825d-218b1b6ec9b5

--- ezinfo.php
+++ ezinfo.php
@@ -13,7 +13,7 @@ class eZCommentsInfo
     {
         return array(
             'Name' => "<a href='http://projects.ez.no/ezcomments'>eZ Comments</a>",
-            'Version' => "1.0.0",
+            'Version' => "1.0.1",
             'Copyright' => 'Copyright (C) 1999-2010 eZ Systems AS',
             'License' => 'GNU General Public License v2.0',
         );
