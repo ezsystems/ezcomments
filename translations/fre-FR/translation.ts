9b2b939e28a231688018107edae6f5dc7e64658e
Jérôme Vieilledent <lolautruche@gmail.com>
2011-02-25 10:36:10 +0100

    Added french translation

---
+++ translations/fre-FR/translation.ts
@@ -0,0 +1,445 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>design/ezwebin/user/edit</name>
+    <message>
+        <source>User profile</source>
+        <translation>Profil de l&apos;utilisateur</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation>Nom d&apos;utilisateur</translation>
+    </message>
+    <message>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <source>My drafts</source>
+        <translation>Mes brouillons</translation>
+    </message>
+    <message>
+        <source>My orders</source>
+        <translation>Mes commandes</translation>
+    </message>
+    <message>
+        <source>My pending items</source>
+        <translation>Mes éléments en attente</translation>
+    </message>
+    <message>
+        <source>My notification settings</source>
+        <translation>Mes réglages de notification</translation>
+    </message>
+    <message>
+        <source>My wish list</source>
+        <translation>Ma liste de souhaits</translation>
+    </message>
+    <message>
+        <source>Edit profile</source>
+        <translation>Éditer le profil</translation>
+    </message>
+    <message>
+        <source>Change password</source>
+        <translation>Changer le mot de passe</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/action</name>
+    <message>
+        <source>Back</source>
+        <translation>Retour</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Supprimer</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <source>Update comment</source>
+        <translation>Mettre à jour le commentaire</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/activate</name>
+    <message>
+        <source>There is no subscription with the hash string!</source>
+        <translation>Il n&apos;existe aucun abonnement correspondant à la clé fournie !</translation>
+    </message>
+    <message>
+        <source>The subscriber is disabled!</source>
+        <translation>L&apos;abonnement est désactivé !</translation>
+    </message>
+    <message>
+        <source>The subscription is activated!</source>
+        <translation>L&apos;abonnement est activé !</translation>
+    </message>
+    <message>
+        <source>Go to settings</source>
+        <translation>Aller aux réglages</translation>
+    </message>
+    <message>
+        <source>Activate subscription</source>
+        <translation>Activer l&apos;abonnement</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/activationnotification</name>
+    <message>
+        <source>Dear %email,</source>
+        <translation>Cher %email</translation>
+    </message>
+    <message>
+        <source>You have subscribed to comment update on content. Before you start the service, you need to confirm the subscription.</source>
+        <translation>Vous vous êtes abonné pour recevoir les nouveaux commentaires. Avant de commencer, vous devez confirmer cet abonnement.</translation>
+    </message>
+    <message>
+        <source>Please click the link to activate.</source>
+        <translation>Merci de cliquer sur lien afin d&apos;effectuer l&apos;activation.</translation>
+    </message>
+    <message>
+        <source>The activation will expire after %expiry_days days.</source>
+        <translation>L&apos;activation expirera après %expire_days jours.</translation>
+    </message>
+    <message>
+        <source>The activation will expire after %expiry_hours hours.</source>
+        <translation>L&apos;activation expirera après %expiry_hours heures.</translation>
+    </message>
+    <message>
+        <source>If you do not want to receive the subscription email, ignore this email.</source>
+        <translation>Si vous ne souhaitez pas recevoir de mise à jour, ignorez cet email.</translation>
+    </message>
+    <message>
+        <source>You can go to your setting page to manage your subscription.</source>
+        <translation>Vous pouvez vous rendre sur votre page de réglages afin de gérer votre abonnement.</translation>
+    </message>
+    <message>
+        <source>Activate your subscription</source>
+        <translation>Activer votre abonnement</translation>
+    </message>
+    <message>
+        <source>posted a new comment on</source>
+        <translation>a posté un nouveau commentaire sur</translation>
+    </message>
+    <message>
+        <source>Comment summary</source>
+        <translation>Résumé du commentaire</translation>
+    </message>
+    <message>
+        <source>To reply the content, please visit</source>
+        <translation>Pour répondre, merci de vous rendre à</translation>
+    </message>
+    <message>
+        <source>For setting your subscription, please visit</source>
+        <translation>Pour gérer votre abonnement, merci de vous rendre à</translation>
+    </message>
+    <message>
+        <source>(You need to login)</source>
+        <translation>(Vous devez vous connecter)</translation>
+    </message>
+    <message>
+        <source>There are updates from</source>
+        <translation>Des mises à jour sont disponibles pour</translation>
+    </message>
+    <message>
+        <source>Hi,</source>
+        <translation>Bonjour</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/add</name>
+    <message>
+        <source>Not a valid email address.</source>
+        <translation>Adresse email invalide.</translation>
+    </message>
+    <message>
+        <source>%1 is missing.</source>
+        <translation>%1 est absent.</translation>
+    </message>
+    <message>
+        <source>The field [%1] is empty.</source>
+        <translation>Le champ [%1] est vide.</translation>
+    </message>
+    <message>
+        <source>Your comment has been posted.</source>
+        <translation>Votre commentaire a été posté.</translation>
+    </message>
+    <message>
+        <source>Add comment</source>
+        <translation>Ajouter un commentaire</translation>
+    </message>
+    <message>
+        <source>Commenting has been turned off for this content.</source>
+        <translation>Les commentaires ont été désactivés pour ce contenu.</translation>
+    </message>
+    <message>
+        <source>You have already subscribed to comment updates on this content.</source>
+        <translation>Vous êtes déjà abonné(e) aux commentaires de ce contenu.</translation>
+    </message>
+    <message>
+        <source>You will receive comment updates on the content.</source>
+        <translation>Vous recevrez les nouveaux commentaires pour ce contenu.</translation>
+    </message>
+    <message>
+        <source>A confirmation email has been sent to your email address. You will receive comment updates after confirmation.</source>
+        <translation>Un email de confirmation vient d&apos;être envoyé à votre adresse email. Vous recevrez les nouveaux commentaires après confirmation.</translation>
+    </message>
+    <message>
+        <source>You should not access this view directly.</source>
+        <translation>Vous de pouvez pas accéder directement à cette vue.</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/add/form</name>
+    <message>
+        <source>Post comment</source>
+        <translation>Ajouter un commentaire</translation>
+    </message>
+    <message>
+        <source>Title:</source>
+        <translation>Titre :</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>Nom :</translation>
+    </message>
+    <message>
+        <source>Website:</source>
+        <translation>Site web :</translation>
+    </message>
+    <message>
+        <source>Email:</source>
+        <translation>Email :</translation>
+    </message>
+    <message>
+        <source>(The email address will not be shown)</source>
+        <translation>(L&apos;adresse email ne sera pas affichée)</translation>
+    </message>
+    <message>
+        <source>Content:</source>
+        <translation>Contenu :</translation>
+    </message>
+    <message>
+        <source>Notify me of new comments</source>
+        <translation>M&apos;avertir des nouveaux commentaires</translation>
+    </message>
+    <message>
+        <source>Remember me</source>
+        <translation>Se souvenir de moi</translation>
+    </message>
+    <message>
+        <source>Add comment</source>
+        <translation>Ajouter un commentaire</translation>
+    </message>
+    <message>
+        <source>Reset form</source>
+        <translation>Réinitialiser le formulaire</translation>
+    </message>
+    <message>
+        <source>Notified of new comments</source>
+        <translation>M&apos;avertir des nouveaux commentaires</translation>
+    </message>
+    <message>
+        <source>There is a problem with your comment form </source>
+        <translation>Un problème est survenu avec votre commentaire</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/delete</name>
+    <message>
+        <source>Delete comment?</source>
+        <translation>Supprimer le commentaire ?</translation>
+    </message>
+    <message>
+        <source>Deleting failed!</source>
+        <translation>La suppression a échoué !</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to delete comment or the comment is hidden!</source>
+        <translation>Vous ne pouvez pas supprimer ce commentaire ou le commentaire est caché !</translation>
+    </message>
+    <message>
+        <source>Delete comment</source>
+        <translation>Supprimer le commentaire</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/edit</name>
+    <message>
+        <source>You don&apos;t have access to edit.</source>
+        <translation>Vous ne pouvez pas éditer.</translation>
+    </message>
+    <message>
+        <source>Edit comment</source>
+        <translation>Éditer le commentaire</translation>
+    </message>
+    <message>
+        <source>Updating failed.</source>
+        <translation>La mise à jour à échoué.</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/notification</name>
+    <message>
+        <source>New comment</source>
+        <translation>Nouveau commentaire</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/setting</name>
+    <message>
+        <source>The subscriber doesn&apos;t exist.</source>
+        <translation>L&apos;abonné n&apos;existe pas.</translation>
+    </message>
+    <message>
+        <source>You are not subscribing to any content.</source>
+        <translation>Vous n&apos;êtes abonné à aucun contenu.</translation>
+    </message>
+    <message>
+        <source>You have updated comment settings.</source>
+        <translation>Vous avez bien mis à jour vos réglages de commentaires.</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Sélectionnez</translation>
+    </message>
+    <message>
+        <source>Content</source>
+        <translation>Contenu</translation>
+    </message>
+    <message>
+        <source>Subscription started</source>
+        <translation>L&apos;abonnement a commencé</translation>
+    </message>
+    <message>
+        <source>Subscribed</source>
+        <translation>Abonné</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Enregistrer</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Réinitialiser</translation>
+    </message>
+    <message>
+        <source>Comment settings</source>
+        <translation>Réglages des commentaires</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/view</name>
+    <message>
+        <source>wrote:</source>
+        <translation>a écrit:</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Éditer</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Supprimer</translation>
+    </message>
+    <message>
+        <source>There is no comment.</source>
+        <translation>Il n&apos;y a aucun commentaire.</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to view comment.</source>
+        <translation>Vous ne pouvez pas voir ce commentaire.</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to post comment.</source>
+        <translation>Vous ne pouvez pas poster de commentaire.</translation>
+    </message>
+    <message>
+        <source>View all %total_count comments</source>
+        <translation>Lire tous les %total_count commentaires</translation>
+    </message>
+    <message>
+        <source>Total %total_count comments</source>
+        <translation>%total_count commentaires au total</translation>
+    </message>
+    <message>
+        <source>The page parameter is not a number.</source>
+        <translation>Le paramètre de page n&apos;est pas un nombre.</translation>
+    </message>
+    <message>
+        <source>The page parameter can&apos;t be minus.</source>
+        <translation>Le paramètre de page ne peut pas être négatif.</translation>
+    </message>
+    <message>
+        <source>Comment/View</source>
+        <translation>Comment/View</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/view/action</name>
+    <message>
+        <source>Back</source>
+        <translation>Retour</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/view/page</name>
+    <message>
+        <source>%current_page/%total_page</source>
+        <translation>%current_page/%total_page</translation>
+    </message>
+    <message>
+        <source>Previous page</source>
+        <translation>Page précédente</translation>
+    </message>
+    <message>
+        <source>Next page</source>
+        <translation>Page suivante</translation>
+    </message>
+    <message>
+        <source>Total comments:</source>
+        <translation>Nombre total de commentaires :</translation>
+    </message>
+    <message>
+        <source>Pages:</source>
+        <translation>Pages :</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/datatype</name>
+    <message>
+        <source>Comments</source>
+        <comment>Datatype name</comment>
+        <translation>Commentaires</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/datatype/edit</name>
+    <message>
+        <source>Show comments</source>
+        <translation>Afficher les commentaires</translation>
+    </message>
+    <message>
+        <source>Enable commenting</source>
+        <translation>Activer les commentaires</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/ezwebin/user/edit</name>
+    <message>
+        <source>My comment settings</source>
+        <translation>Mes réglages pour les commentaires</translation>
+    </message>
+</context>
+</TS>
