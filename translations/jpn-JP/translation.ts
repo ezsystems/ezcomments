cd7da0c67396d2e486f57e11ea31ed03d965804e
Eric Sagnes <es@ez.no>
2011-03-08 16:38:32 +0800

    localization: updated jpn-JP translation.ts

---
+++ translations/jpn-JP/translation.ts
@@ -0,0 +1,531 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ja_JP">
+<context>
+    <name>design/ezwebin/user/edit</name>
+    <message>
+        <source>User profile</source>
+        <translation>ユーザプロフィール</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation>ユーザ名</translation>
+    </message>
+    <message>
+        <source>Email</source>
+        <translation>メール</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>名前</translation>
+    </message>
+    <message>
+        <source>My drafts</source>
+        <translation>ドラフト</translation>
+    </message>
+    <message>
+        <source>My orders</source>
+        <translation>注文</translation>
+    </message>
+    <message>
+        <source>My pending items</source>
+        <translation>保留アイテム</translation>
+    </message>
+    <message>
+        <source>My notification settings</source>
+        <translation>通知設定</translation>
+    </message>
+    <message>
+        <source>My wish list</source>
+        <translation>ウィッシュリスト</translation>
+    </message>
+    <message>
+        <source>Edit profile</source>
+        <translation>プロフィール編集</translation>
+    </message>
+    <message>
+        <source>Change password</source>
+        <translation>パスワード変更</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/class/edit</name>
+    <message>
+        <source>eZ Comments warning: the reCAPTCHA key is not set up properly.</source>
+        <translation>eZ Comments警告: reCAPTCHAキーは正しく設定されていません。</translation>
+    </message>
+    <message>
+        <source>Please get a reCAPTCHA key from &lt;a href=&quot;https://admin.recaptcha.net/recaptcha/createsite&quot; target=&quot;_blank&quot;&gt;https://admin.recaptcha.net/recaptcha/createsite&lt;/a&gt; then set it up in eZ Comments, or disable CAPTCHA feature.</source>
+        <translation>&lt;a href=&quot;https://admin.recaptcha.net/recaptcha/createsite&quot; target=&quot;_blank&quot;&gt;https://admin.recaptcha.net/recaptcha/createsite&lt;/a&gt;からreCAPTCHAキーをもらって、eZ Commentsで設定してください。また、キャプチャ機能を無効にすることができます。</translation>
+    </message>
+    <message>
+        <source>For more details please visit &lt;a href=&quot;http://projects.ez.no/ezcomments&quot; target=&quot;_blank&quot;&gt;http://projects.ez.no/ezcomments&lt;/a&gt;.</source>
+        <translation>詳細情報に&lt;a href=&quot;http://projects.ez.no/ezcomments&quot; target=&quot;_blank&quot;&gt;http://projects.ez.no/ezcomments&lt;/a&gt;を参照してください。</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/action</name>
+    <message>
+        <source>Back</source>
+        <translation>前</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>削除</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>キャンセル</translation>
+    </message>
+    <message>
+        <source>Update comment</source>
+        <translation>コメント更新</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/activate</name>
+    <message>
+        <location filename="ezcomsubscriptionmanager.php" line="41"/>
+        <source>There is no subscription with the hash string!</source>
+        <translation>ハッシュ文字列を持った通知登録はありません!</translation>
+    </message>
+    <message>
+        <location filename="ezcomsubscriptionmanager.php" line="60"/>
+        <source>The subscriber is disabled!</source>
+        <translation>通知登録者は無効です!</translation>
+    </message>
+    <message>
+        <source>The subscription is activated!</source>
+        <translation>通知登録は有効になりました!</translation>
+    </message>
+    <message>
+        <source>Go to settings</source>
+        <translation>設定へ</translation>
+    </message>
+    <message>
+        <location filename="activate.php" line="27"/>
+        <source>Activate subscription</source>
+        <translation>通知登録を有効にする</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/activationnotification</name>
+    <message>
+        <source>Dear %email,</source>
+        <translation>%emailさん</translation>
+    </message>
+    <message>
+        <source>You have subscribed to comment update on content. Before you start the service, you need to confirm the subscription.</source>
+        <translation>コンテンツのコメント更新の通知に登録しました。サービスの開始前に通知設定を確認する必要があります。</translation>
+    </message>
+    <message>
+        <source>Please click the link to activate.</source>
+        <translation>有効にするためにはリンクをクリックしてください。</translation>
+    </message>
+    <message>
+        <source>The activation will expire after %expiry_days days.</source>
+        <translation>有効リンクは%expiry_hours時間後に無効となります。</translation>
+    </message>
+    <message>
+        <source>The activation will expire after %expiry_hours hours.</source>
+        <translation>有効リンクは%expiry_hours時間後に無効となります。</translation>
+    </message>
+    <message>
+        <source>If you do not want to receive the subscription email, ignore this email.</source>
+        <translation>通知メールが不要の場合にこのメールを無視してください。</translation>
+    </message>
+    <message>
+        <source>You can go to your setting page to manage your subscription.</source>
+        <translation>設定ページに、通知登録を管理できます。</translation>
+    </message>
+    <message>
+        <source>Activate your subscription</source>
+        <translation>通知登録を有効にする</translation>
+    </message>
+    <message>
+        <source>Hi,</source>
+        <translation>こんにちは、</translation>
+    </message>
+    <message>
+        <source>posted a new comment on</source>
+        <translation>次のページに新しいコメント</translation>
+    </message>
+    <message>
+        <source>Comment summary</source>
+        <translation>コメントサマリ</translation>
+    </message>
+    <message>
+        <source>To reply the content, please visit</source>
+        <translation>コンテンツに返事する次に行ってください</translation>
+    </message>
+    <message>
+        <source>For setting your subscription, please visit</source>
+        <translation>通知設定には次のリンクを参照にしてください</translation>
+    </message>
+    <message>
+        <source>(You need to login)</source>
+        <translation>（ログイン必要）</translation>
+    </message>
+    <message>
+        <source>There are updates from</source>
+        <translation>次のオブジェクトの更新通知となります</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/add</name>
+    <message>
+        <location filename="ezcomaddcommenttool.php" line="66"/>
+        <source>Not a valid email address.</source>
+        <translation>有効なメールアドレスではありません。</translation>
+    </message>
+    <message>
+        <location filename="ezcomaddcommenttool.php" line="84"/>
+        <source>The words you input are incorrect.</source>
+        <translation>入力した言葉が不正です。</translation>
+    </message>
+    <message>
+        <location filename="ezcomaddcommenttool.php" line="89"/>
+        <source>Captcha parameter error.</source>
+        <translation>キャプチャパラメターエラー。</translation>
+    </message>
+    <message>
+        <location filename="ezcomformtool.php" line="96"/>
+        <source>%1 is missing.</source>
+        <translation>%1が不足しています。</translation>
+    </message>
+    <message>
+        <location filename="ezcomformtool.php" line="109"/>
+        <source>The field [%1] is empty.</source>
+        <translation>[%1]フィールドは空。</translation>
+    </message>
+    <message>
+        <location filename="ezcomutility.php" line="23"/>
+        <source>Javascript code in url is not allowed.</source>
+        <translation>URLにJavascriptコードは禁断されています。</translation>
+    </message>
+    <message>
+        <location filename="ezcomutility.php" line="27"/>
+        <source>Email link in url is not allowed.</source>
+        <translation>URLにメールリンクは禁断されています。</translation>
+    </message>
+    <message>
+        <source>Your comment has been posted.</source>
+        <translation>コメントが投稿されました。</translation>
+    </message>
+    <message>
+        <source>reCAPTCHA API key missing.</source>
+        <translation>reCAPTCHA APIのキーがありません。</translation>
+    </message>
+    <message>
+        <location filename="add.php" line="22"/>
+        <source>Add comment</source>
+        <translation>コメントの追加</translation>
+    </message>
+    <message>
+        <location filename="add.php" line="43"/>
+        <source>Commenting has been turned off for this content.</source>
+        <translation>このコンテンツにコメントは無効です。</translation>
+    </message>
+    <message>
+        <location filename="add.php" line="149"/>
+        <source>You have already subscribed to comment updates on this content.</source>
+        <translation>このコンテンツのコメント更新通知にすでに登録しました。</translation>
+    </message>
+    <message>
+        <location filename="add.php" line="154"/>
+        <source>You will receive comment updates on the content.</source>
+        <translation>コメント更新通知が届きます。</translation>
+    </message>
+    <message>
+        <location filename="add.php" line="160"/>
+        <source>A confirmation email has been sent to your email address. You will receive comment updates after confirmation.</source>
+        <translation>確認のメールをアドレスに送りました。確認後にコメント更新通知をもらいます。</translation>
+    </message>
+    <message>
+        <location filename="add.php" line="197"/>
+        <source>You should not access this view directly.</source>
+        <translation>このビューに直接アクセスしない様にしてください。</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/add/form</name>
+    <message>
+        <source>Post comment</source>
+        <translation>コメントを投稿</translation>
+    </message>
+    <message>
+        <source>Title:</source>
+        <translation>タイトル:</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>名前:</translation>
+    </message>
+    <message>
+        <source>Website:</source>
+        <translation>ウェブサイト:</translation>
+    </message>
+    <message>
+        <source>Email:</source>
+        <translation>メール:</translation>
+    </message>
+    <message>
+        <source>(The email address will not be shown)</source>
+        <translation>(メールアドレスは表示されません)</translation>
+    </message>
+    <message>
+        <source>Content:</source>
+        <translation>コンテント:</translation>
+    </message>
+    <message>
+        <source>Notify me of new comments</source>
+        <translation>新規コメントの通知</translation>
+    </message>
+    <message>
+        <source>Security text:</source>
+        <translation>セキュリティテキスト:</translation>
+    </message>
+    <message>
+        <source>Enter both words below, with or without a space.</source>
+        <translation>二つの言葉を下記に入力してください。</translation>
+    </message>
+    <message>
+        <source>The letters are not case-sensitive.</source>
+        <translation>大文字と小文字は区別されません。</translation>
+    </message>
+    <message>
+        <source>Can&apos;t read this?</source>
+        <translation>読めれない？</translation>
+    </message>
+    <message>
+        <source>Try another</source>
+        <translation>変更する</translation>
+    </message>
+    <message>
+        <source>Remember me</source>
+        <translation>情報を覚える</translation>
+    </message>
+    <message>
+        <source>Add comment</source>
+        <translation>コメントの追加</translation>
+    </message>
+    <message>
+        <source>Reset form</source>
+        <translation>フォームのリセット</translation>
+    </message>
+    <message>
+        <source>Notified of new comments</source>
+        <translation>新規コメントの通知設定有効</translation>
+    </message>
+    <message>
+        <location filename="add.php" line="56"/>
+        <location filename="edit.php" line="80"/>
+        <source>There is a problem with your comment form </source>
+        <translation>コメントフォームに問題があります</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/delete</name>
+    <message>
+        <source>Delete comment?</source>
+        <translation>コメントを削除する？</translation>
+    </message>
+    <message>
+        <location filename="delete.php" line="54"/>
+        <source>Deleting failed!</source>
+        <translation>削除が失敗しました！</translation>
+    </message>
+    <message>
+        <location filename="delete.php" line="105"/>
+        <source>You don&apos;t have access to delete comment or the comment is hidden!</source>
+        <translation>このコメントを削除する権限ありませんかコメントが非表示になっています!</translation>
+    </message>
+    <message>
+        <location filename="delete.php" line="141"/>
+        <source>Delete comment</source>
+        <translation>コメント削除</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/edit</name>
+    <message>
+        <source>You don&apos;t have access to edit.</source>
+        <translation>編集の権限ありません。</translation>
+    </message>
+    <message>
+        <location filename="edit.php" line="49"/>
+        <location filename="edit.php" line="151"/>
+        <source>Edit comment</source>
+        <translation>コメント編集</translation>
+    </message>
+    <message>
+        <location filename="edit.php" line="108"/>
+        <source>Updating failed.</source>
+        <translation>更新失敗。</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/notification</name>
+    <message>
+        <source>New comment</source>
+        <translation>新規コメント</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/setting</name>
+    <message>
+        <source>The subscriber doesn&apos;t exist.</source>
+        <translation>通知登録者は存在しません。</translation>
+    </message>
+    <message>
+        <source>You are not subscribing to any content.</source>
+        <translation>コンテンツに通知登録をしていません。</translation>
+    </message>
+    <message>
+        <source>You have updated comment settings.</source>
+        <translation>コメント設定を更新しました。</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>選択</translation>
+    </message>
+    <message>
+        <source>Content</source>
+        <translation>コンテンツ</translation>
+    </message>
+    <message>
+        <source>Subscription started</source>
+        <translation>通知開始</translation>
+    </message>
+    <message>
+        <source>Subscribed</source>
+        <translation>登録済み</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>リセット</translation>
+    </message>
+    <message>
+        <location filename="setting.php" line="31"/>
+        <location filename="setting.php" line="66"/>
+        <location filename="setting.php" line="134"/>
+        <source>Comment settings</source>
+        <translation>コメント設定</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/view</name>
+    <message>
+        <source>wrote:</source>
+        <translation>書きました:</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>編集</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>削除</translation>
+    </message>
+    <message>
+        <source>There is no comment.</source>
+        <translation>コメントがありません。</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to view comment.</source>
+        <translation>コメントを表示できる権限がありません。</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to post comment.</source>
+        <translation>コメントを投稿できる権限がありません。</translation>
+    </message>
+    <message>
+        <source>View all %total_count comments</source>
+        <translation>%total_countコメントを見る</translation>
+    </message>
+    <message>
+        <source>Total %total_count comments</source>
+        <translation>合計%total_countコメント</translation>
+    </message>
+    <message>
+        <location filename="view.php" line="69"/>
+        <source>The page parameter is not a number.</source>
+        <translation>ページパラメターはすっ辞でありません。</translation>
+    </message>
+    <message>
+        <location filename="view.php" line="92"/>
+        <source>The page parameter can&apos;t be minus.</source>
+        <translation>ページパラメターはマイナスにできません。</translation>
+    </message>
+    <message>
+        <location filename="view.php" line="117"/>
+        <source>Comment/View</source>
+        <translation>コメント/表示</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/view/action</name>
+    <message>
+        <source>Back</source>
+        <translation>前</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/view/page</name>
+    <message>
+        <source>%current_page/%total_page</source>
+        <translation>%current_page/%total_page</translation>
+    </message>
+    <message>
+        <source>Previous page</source>
+        <translation>前のページ</translation>
+    </message>
+    <message>
+        <source>Next page</source>
+        <translation>次のページ</translation>
+    </message>
+    <message>
+        <source>Total comments:</source>
+        <translation>コメント数:</translation>
+    </message>
+    <message>
+        <source>Pages:</source>
+        <translation>ページ:</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/datatype</name>
+    <message>
+        <location filename="ezcomcommentstype.php" line="20"/>
+        <source>Comments</source>
+        <comment>Datatype name</comment>
+        <translation>コメント</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/datatype/edit</name>
+    <message>
+        <source>Show comments</source>
+        <translation>コメントを表示</translation>
+    </message>
+    <message>
+        <source>Enable commenting</source>
+        <translation>コメントを有効</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/ezwebin/user/edit</name>
+    <message>
+        <source>My comment settings</source>
+        <translation>コメント設定</translation>
+    </message>
+</context>
+</TS>
