ecc76a0910de5832438b09950551e9f8a4cde4ea
Edi Modric <edi.modric@gmail.com>
2011-03-10 18:51:39 +0800

    Croatian translation

---
+++ translations/cro-HR/translation.ts
@@ -0,0 +1,500 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>design/ezwebin/user/edit</name>
+    <message>
+        <source>User profile</source>
+        <translation>Korisnički profil</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation>Korisničko ime</translation>
+    </message>
+    <message>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Ime</translation>
+    </message>
+    <message>
+        <source>My drafts</source>
+        <translation>Moje skice</translation>
+    </message>
+    <message>
+        <source>My orders</source>
+        <translation>Moje narudžbe</translation>
+    </message>
+    <message>
+        <source>My pending items</source>
+        <translation>Moje stavke za obradu</translation>
+    </message>
+    <message>
+        <source>My notification settings</source>
+        <translation>Postavke obavijesti</translation>
+    </message>
+    <message>
+        <source>My wish list</source>
+        <translation>Moja lista želja</translation>
+    </message>
+    <message>
+        <source>Edit profile</source>
+        <translation>Uredi profil</translation>
+    </message>
+    <message>
+        <source>Change password</source>
+        <translation>Promijeni lozinku</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/class/edit</name>
+    <message>
+        <source>eZ Comments warning: the reCAPTCHA key is not set up properly.</source>
+        <translation>eZ Comments upozorenje: reCAPTCHA ključ nije ispravan.</translation>
+    </message>
+    <message>
+        <source>Please get a reCAPTCHA key from &lt;a href=&quot;https://admin.recaptcha.net/recaptcha/createsite&quot; target=&quot;_blank&quot;&gt;https://admin.recaptcha.net/recaptcha/createsite&lt;/a&gt; then set it up in eZ Comments, or disable CAPTCHA feature.</source>
+        <translation>Zatražite reCAPTCHA ključ na &lt;a href=&quot;https://admin.recaptcha.net/recaptcha/createsite&quot; target=&quot;_blank&quot;&gt;https://admin.recaptcha.net/recaptcha/createsite&lt;/a&gt; i upišite ga u postavke, ili onemogućite CAPTCHA funkcionalnost.</translation>
+    </message>
+    <message>
+        <source>For more details please visit &lt;a href=&quot;http://projects.ez.no/ezcomments&quot; target=&quot;_blank&quot;&gt;http://projects.ez.no/ezcomments&lt;/a&gt;.</source>
+        <translation> Za više detalja posjetite &lt;a href=&quot;http://projects.ez.no/ezcomments&quot; target=&quot;_blank&quot;&gt;http://projects.ez.no/ezcomments&lt;/a&gt;.</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/action</name>
+    <message>
+        <source>Back</source>
+        <translation>Natrag</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Obriši</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Odustani</translation>
+    </message>
+    <message>
+        <source>Update comment</source>
+        <translation>Uredi komentar</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/activate</name>
+    <message>
+        <source>There is no subscription with the hash string!</source>
+        <translation>Ne postoji pretplata s tim sažetkom!</translation>
+    </message>
+    <message>
+        <source>The subscriber is disabled!</source>
+        <translation>Pretplatnik je onemogućen!</translation>
+    </message>
+    <message>
+        <source>The subscription is activated!</source>
+        <translation>Pretplata je aktivirana!</translation>
+    </message>
+    <message>
+        <source>Go to settings</source>
+        <translation>Postavke</translation>
+    </message>
+    <message>
+        <source>Activate subscription</source>
+        <translation>Aktiviraj pretplatu</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/activationnotification</name>
+    <message>
+        <source>Dear %email,</source>
+        <translation>Dragi %email,</translation>
+    </message>
+    <message>
+        <source>You have subscribed to comment update on content. Before you start the service, you need to confirm the subscription.</source>
+        <translation>Pretplatili ste se na obavijesti o novim komentarima. Morate potvrditi vašu pretplatu.</translation>
+    </message>
+    <message>
+        <source>Please click the link to activate.</source>
+        <translation>Kliknite na link za aktivaciju.</translation>
+    </message>
+    <message>
+        <source>The activation will expire after %expiry_days days.</source>
+        <translation>Aktivacija će isteći nakon %expiry_days dana.</translation>
+    </message>
+    <message>
+        <source>The activation will expire after %expiry_hours hours.</source>
+        <translation>Aktivacija će isteći nakon %expiry_days sati.</translation>
+    </message>
+    <message>
+        <source>If you do not want to receive the subscription email, ignore this email.</source>
+        <translation>Ako ne želite primati pretplatne email poruke, ignorirajte ovaj email.</translation>
+    </message>
+    <message>
+        <source>You can go to your setting page to manage your subscription.</source>
+        <translation>Možete upravljati vašom pretplatom na stranicama postavki.</translation>
+    </message>
+    <message>
+        <source>Activate your subscription</source>
+        <translation>Aktivirajte pretplatu</translation>
+    </message>
+    <message>
+        <source>posted a new comment on</source>
+        <translation>je dodao novi komentar na</translation>
+    </message>
+    <message>
+        <source>Comment summary</source>
+        <translation>Sažetak komentara</translation>
+    </message>
+    <message>
+        <source>To reply the content, please visit</source>
+        <translation>Da biste odgovorili, posjetite</translation>
+    </message>
+    <message>
+        <source>For setting your subscription, please visit</source>
+        <translation>Za podešavanje vaše pretplate, posjetite</translation>
+    </message>
+    <message>
+        <source>(You need to login)</source>
+        <translation>(Trebate se prijaviti)</translation>
+    </message>
+    <message>
+        <source>There are updates from</source>
+        <translation>Postoje najnovije informacije od</translation>
+    </message>
+    <message>
+        <source>Hi,</source>
+        <translation>Bok,</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/add</name>
+    <message>
+        <source>Not a valid email address.</source>
+        <translation>Neispravna email adresa.</translation>
+    </message>
+    <message>
+        <source>%1 is missing.</source>
+        <translation>Nedostaje %1.</translation>
+    </message>
+    <message>
+        <source>The field [%1] is empty.</source>
+        <translation>Polje [%1] je prazno.</translation>
+    </message>
+    <message>
+        <source>Your comment has been posted.</source>
+        <translation>Vaš komentar je dodan.</translation>
+    </message>
+    <message>
+        <source>Add comment</source>
+        <translation>Dodaj komentar</translation>
+    </message>
+    <message>
+        <source>Commenting has been turned off for this content.</source>
+        <translation>Komentiranje nije dozvoljeno za ovaj sadržaj.</translation>
+    </message>
+    <message>
+        <source>You have already subscribed to comment updates on this content.</source>
+        <translation>Već ste se pretplatili na obavijesti o novim komentarima za ovaj sadržaj.</translation>
+    </message>
+    <message>
+        <source>You will receive comment updates on the content.</source>
+        <translation>Primat ćete obavijesti o novim komentarima za ovaj sadržaj.</translation>
+    </message>
+    <message>
+        <source>A confirmation email has been sent to your email address. You will receive comment updates after confirmation.</source>
+        <translation>Potvrdni email je poslan na vašu email adresu. Primat ćete obavijesti o novim komentarima nakon što potvrdite vašu email adresu.</translation>
+    </message>
+    <message>
+        <source>You should not access this view directly.</source>
+        <translation>Ne biste trebali direktno pristupati ovom pogledu.</translation>
+    </message>
+    <message>
+        <source>reCAPTCHA API key missing.</source>
+        <translation>Nedostaje reCAPTCHA API ključ</translation>
+    </message>
+    <message>
+        <source>The words you input are incorrect.</source>
+        <translation>Unesene riječi su neispravne.</translation>
+    </message>
+    <message>
+        <source>Captcha parameter error.</source>
+        <translation>Greška u Captcha parametrima.</translation>
+    </message>
+    <message>
+        <source>Javascript code in url is not allowed.</source>
+        <translation>Javascript kod u URLu nije dozvoljen.</translation>
+    </message>
+    <message>
+        <source>Email link in url is not allowed.</source>
+        <translation>Email link u URLu nije dozvoljen.</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/add/form</name>
+    <message>
+        <source>Post comment</source>
+        <translation>Objava komentara</translation>
+    </message>
+    <message>
+        <source>Title:</source>
+        <translation>Naslov:</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>Ime:</translation>
+    </message>
+    <message>
+        <source>Website:</source>
+        <translation>Web stranica:</translation>
+    </message>
+    <message>
+        <source>Email:</source>
+        <translation>Email:</translation>
+    </message>
+    <message>
+        <source>(The email address will not be shown)</source>
+        <translation>(Email adresa neće biti prikazana)</translation>
+    </message>
+    <message>
+        <source>Content:</source>
+        <translation>Tekst:</translation>
+    </message>
+    <message>
+        <source>Notify me of new comments</source>
+        <translation>Obavijesti me o novim komentarima</translation>
+    </message>
+    <message>
+        <source>Remember me</source>
+        <translation>Zapamti me</translation>
+    </message>
+    <message>
+        <source>Add comment</source>
+        <translation>Dodaj komentar</translation>
+    </message>
+    <message>
+        <source>Reset form</source>
+        <translation>Obriši formu</translation>
+    </message>
+    <message>
+        <source>Notified of new comments</source>
+        <translation>Obavijesti o novim komentarima aktivne</translation>
+    </message>
+    <message>
+        <source>There is a problem with your comment form </source>
+        <translation>Postoji problem s formom </translation>
+    </message>
+    <message>
+        <source>Enter both words below, with or without a space.</source>
+        <translation>Unesite obje riječi, s ili bez razmaka.</translation>
+    </message>
+    <message>
+        <source>The letters are not case-sensitive.</source>
+        <translation>Možete unositi i velika i mala slova.</translation>
+    </message>
+    <message>
+        <source>Can&apos;t read this?</source>
+        <translation>Ne možete pročitati ovo?</translation>
+    </message>
+    <message>
+        <source>Try another</source>
+        <translation>Druga slika</translation>
+    </message>
+    <message>
+        <source>Security text:</source>
+        <translation>Sigurnosni tekst:</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/delete</name>
+    <message>
+        <source>Delete comment?</source>
+        <translation>Obrisati komentar?</translation>
+    </message>
+    <message>
+        <source>Deleting failed!</source>
+        <translation>Brisanje neuspjelo!</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to delete comment or the comment is hidden!</source>
+        <translation>Nemate dozvole za brisanje komentara ili je komentar skriven!</translation>
+    </message>
+    <message>
+        <source>Delete comment</source>
+        <translation>Obriši komentar</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/edit</name>
+    <message>
+        <source>You don&apos;t have access to edit.</source>
+        <translation>Nemate dozvole za uređivanje.</translation>
+    </message>
+    <message>
+        <source>Edit comment</source>
+        <translation>Uredi komentar.</translation>
+    </message>
+    <message>
+        <source>Updating failed.</source>
+        <translation>Uređivanje neuspjelo.</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/notification</name>
+    <message>
+        <source>New comment</source>
+        <translation>Novi komentar</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/setting</name>
+    <message>
+        <source>The subscriber doesn&apos;t exist.</source>
+        <translation>Pretplatnik ne postoji.</translation>
+    </message>
+    <message>
+        <source>You are not subscribing to any content.</source>
+        <translation>Niste pretplaćeni na obavijesti o novim komentarima.</translation>
+    </message>
+    <message>
+        <source>You have updated comment settings.</source>
+        <translation>Uspješno ste ažurirali postavke komentara.</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Odaberi</translation>
+    </message>
+    <message>
+        <source>Content</source>
+        <translation>Sadržaj</translation>
+    </message>
+    <message>
+        <source>Subscription started</source>
+        <translation>Pretplata aktivirana</translation>
+    </message>
+    <message>
+        <source>Subscribed</source>
+        <translation>Pretplaćen</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Spremi</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Resetiraj</translation>
+    </message>
+    <message>
+        <source>Comment settings</source>
+        <translation>Postavke komentara</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/view</name>
+    <message>
+        <source>wrote:</source>
+        <translation>napisao:</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Uredi</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Obriši</translation>
+    </message>
+    <message>
+        <source>There is no comment.</source>
+        <translation>Nema komentara.</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to view comment.</source>
+        <translation>Nemate dozvole za pregled komentara.</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to post comment.</source>
+        <translation>Nemate dozvole za objavu komentara.</translation>
+    </message>
+    <message>
+        <source>View all %total_count comments</source>
+        <translation>Pogledaj svih %total_count komentara</translation>
+    </message>
+    <message>
+        <source>Total %total_count comments</source>
+        <translation>Ukupno %total_count komentara</translation>
+    </message>
+    <message>
+        <source>The page parameter is not a number.</source>
+        <translation>Parametar stranice nije broj.</translation>
+    </message>
+    <message>
+        <source>The page parameter can&apos;t be minus.</source>
+        <translation>Parametar stranice ne može biti negativan.</translation>
+    </message>
+    <message>
+        <source>Comment/View</source>
+        <translation>Komentiraj/Pregledaj</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/view/action</name>
+    <message>
+        <source>Back</source>
+        <translation>Natrag</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/comment/view/page</name>
+    <message>
+        <source>%current_page/%total_page</source>
+        <translation>%current_page/%total_page</translation>
+    </message>
+    <message>
+        <source>Previous page</source>
+        <translation>Prethodna stranica</translation>
+    </message>
+    <message>
+        <source>Next page</source>
+        <translation>Sljedeća stranica</translation>
+    </message>
+    <message>
+        <source>Total comments:</source>
+        <translation>Ukupno komentara:</translation>
+    </message>
+    <message>
+        <source>Pages:</source>
+        <translation>Stranica:</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/datatype</name>
+    <message>
+        <source>Comments</source>
+        <comment>Datatype name</comment>
+        <translation>Komentari</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/datatype/edit</name>
+    <message>
+        <source>Show comments</source>
+        <translation>Prikazuj komentare</translation>
+    </message>
+    <message>
+        <source>Enable commenting</source>
+        <translation>Omogući komentiranje</translation>
+    </message>
+</context>
+<context>
+    <name>ezcomments/ezwebin/user/edit</name>
+    <message>
+        <source>My comment settings</source>
+        <translation>Moje postavke komentara</translation>
+    </message>
+</context>
+</TS>
