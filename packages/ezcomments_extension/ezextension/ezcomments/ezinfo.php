c159675acbfc9a58284df290e9b7297aed6f5ca1
Damien Pobel <dp@ez.no>
2012-03-29 13:48:41 +0200

    Changed: welcome 2012

--- packages/ezcomments_extension/ezextension/ezcomments/ezinfo.php
+++ packages/ezcomments_extension/ezextension/ezcomments/ezinfo.php
@@ -2,7 +2,7 @@
 /**
  * File containing ezcomCommentsInfo class
  *
- * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
  * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
  *
  */
@@ -14,7 +14,7 @@ class ezcommentsInfo
         return array(
             'Name' => "<a href='http://projects.ez.no/ezcomments'>eZ Comments</a>",
             'Version' => "//autogentag//",
-            'Copyright' => 'Copyright (C) 1999-2011 eZ Systems AS',
+            'Copyright' => 'Copyright (C) 1999-2012 eZ Systems AS',
             'License' => 'GNU General Public License v2.0',
             'Includes the following third-party software' => array( 'Name' => 'reCAPTCHA PHP Library',
                                                                               'Version' => '1.11',
--- packages/ezcomments_extension/ezextension/ezcomments/modules/comment/view.php
+++ packages/ezcomments_extension/ezextension/ezcomments/modules/comment/view.php
@@ -2,7 +2,7 @@
 /**
  * File containing logic of comment view(comment/view in uri)
  *
- * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
  * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
  *
  */
