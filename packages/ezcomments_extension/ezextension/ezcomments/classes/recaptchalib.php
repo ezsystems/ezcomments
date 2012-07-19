17eb4643f52280fbdfb6640132bc1ae7adb6049c
Paul Borgermans <paul.borgermans@gmail.com>
2012-07-19 01:24:18 -0700

    Merge pull request #22 from jjCavalleri/19388
    Fixed bug #019388: eZComments proxy support for captcha(cherry picked from commit e7e441d4cdc645803d2e0103f18103460f2e9952)

--- packages/ezcomments_extension/ezextension/ezcomments/classes/recaptchalib.php
+++ packages/ezcomments_extension/ezextension/ezcomments/classes/recaptchalib.php
@@ -37,7 +37,7 @@
  */
 define("RECAPTCHA_API_SERVER", "http://www.google.com/recaptcha/api");
 define("RECAPTCHA_API_SECURE_SERVER", "https://www.google.com/recaptcha/api");
-define("RECAPTCHA_VERIFY_SERVER", "www.google.com");
+define("RECAPTCHA_VERIFY_SERVER", "http://www.google.com");
 
 /**
  * Encodes the given data into a query string format
@@ -68,25 +68,7 @@ function _recaptcha_http_post($host, $path, $data, $port = 80) {
 
         $req = _recaptcha_qsencode ($data);
 
-        $http_request  = "POST $path HTTP/1.0\r\n";
-        $http_request .= "Host: $host\r\n";
-        $http_request .= "Content-Type: application/x-www-form-urlencoded;\r\n";
-        $http_request .= "Content-Length: " . strlen($req) . "\r\n";
-        $http_request .= "User-Agent: reCAPTCHA/PHP\r\n";
-        $http_request .= "\r\n";
-        $http_request .= $req;
-
-        $response = '';
-        if( false == ( $fs = @fsockopen($host, $port, $errno, $errstr, 10) ) ) {
-                die ('Could not open socket');
-        }
-
-        fwrite($fs, $http_request);
-
-        while ( !feof($fs) )
-                $response .= fgets($fs, 1160); // One TCP-IP packet
-        fclose($fs);
-        $response = explode("\r\n\r\n", $response, 2);
+        $response = eZHTTPTool::getDataByURL( $host . $path . '?' . $req, false, 'reCAPTCHA/PHP' );
 
         return $response;
 }
@@ -178,7 +160,7 @@ function recaptcha_check_answer ($privkey, $remoteip, $challenge, $response, $ex
                                                  ) + $extra_params
                                           );
 
-        $answers = explode ("\n", $response [1]);
+        $answers = explode ("\n", $response );
         $recaptcha_response = new ReCaptchaResponse();
 
         if (trim ($answers [0]) == 'true') {
