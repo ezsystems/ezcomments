66e4838e8a052594137506d620a5618046d5d50b
André R <ar@ez.no>
2013-05-10 14:19:14 +0200

    Change comments menu to in content left menu instead of top
    This change removed the eZ Comments top menu as it is waste of space to have it's own top menu.

--- packages/ezcomments_extension/ezextension/ezcomments/settings/menu.ini.append.php
+++ packages/ezcomments_extension/ezextension/ezcomments/settings/menu.ini.append.php
@@ -1,32 +1,8 @@
 <?php /* #?ini charset="utf-8"?
 
-[NavigationPart]
-Part[ezcommentsnavigationpart]=eZComments
-
-[TopAdminMenu]
-Tabs[]=ezcomments
-
-[Topmenu_ezcomments]
-NavigationPartIdentifier=ezcommentsnavigationpart
-Name=eZ Comments
-Tooltip=eZ Comments dashboard
-URL[]
-URL[default]=comment/list
-Enabled[]
-Enabled[default]=true
-Enabled[browse]=false
-Enabled[edit]=false
-Shown[]
-Shown[default]=true
-Shown[edit]=true
-Shown[navigation]=true
-Shown[browse]=true
-
-PolicyList[]=comment/list
-
-[Leftmenu_ezcomments]
-Name=ezcommentsmenu
-LinkNames[]
-Links[]
+[Leftmenu_content]
 Links[commentlist]=comment/list
+LinkNames[commentlist]=Comments list
+PolicyList_commentlist[]=comment/list
+
 */ ?>
